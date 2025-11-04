14fd18802eb82edc923d1f15ef0c7a1cab70edb5
prazwal-bns <prajwalbns15@gmail.com>
2025-11-04 17:32:02 +0545

    Add PostController and define routes for posts

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/hello', function(){
+    return ["message" => "Hello, World!"];
+});
+
+Route::get('/posts', [App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
+Route::post('/posts', [App\Http\Controllers\PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/{id}', [App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
